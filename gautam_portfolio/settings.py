f7ef14ac2df2d9500431fe7d4389422cf5d14380
Gautam Arora <gautamarora207@gmail.com>
2020-08-25 12:32:28 +0000

    Remove old stuff

--- gautam_portfolio/settings.py
+++ gautam_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = '5o2*u4vl-@qer2bk^s#g477+hck$a0+08!*kx1v)!otf)@pwpd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gautamarora.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
